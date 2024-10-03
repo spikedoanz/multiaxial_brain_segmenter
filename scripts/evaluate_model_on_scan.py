3eb8e6f3da2b31ee6e20429a65d8c86ac9fa3d79
lkshrsch <lukashirsch@gmail.com>
2024-10-03 12:40:07 -0400

    improved prediction script

--- scripts/evaluate_model_on_scan.py
+++ scripts/evaluate_model_on_scan.py
@@ -11,6 +11,7 @@ import numpy as np
 import nibabel as nib
 import tensorflow as tf
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if tf.__version__[0] == '1':
     config = tf.ConfigProto()
@@ -52,59 +53,57 @@ if __name__ == '__main__':
                                      'dice_coef_multilabel_bin5':dice_coef_multilabel_bin5,
                                      'dice_coef_multilabel_bin6':dice_coef_multilabel_bin6}
     
-    SAGITTAL_MODEL_SESSION_PATH = '/home/deeperthought/Projects/Others/2D_brain_segmenter/Sessions/sagittal_segmenter_NoDataAug/'
-    AXIAL_MODEL_SESSION_PATH = '/home/deeperthought/Projects/Others/2D_brain_segmenter/Sessions/axial_segmenter_NoDataAug/'
-    CORONAL_MODEL_SESSION_PATH = '/home/deeperthought/Projects/Others/2D_brain_segmenter/Sessions/coronal_segmenter_NoDataAug/'
+    SAGITTAL_MODEL_SESSION_PATH = '/home/deeperthought/Projects/Multiaxial/Sessions/sagittalSegmenter_PositionalEncoding_2epochs_depth6_baseFilters4/'
+    AXIAL_MODEL_SESSION_PATH = None #'/home/deeperthought/Projects/Others/2D_brain_segmenter/Sessions/axial_segmenter_NoDataAug/'
+    CORONAL_MODEL_SESSION_PATH = None #'/home/deeperthought/Projects/Others/2D_brain_segmenter/Sessions/coronal_segmenter_NoDataAug/'
     
     
     model_sagittal = tf.keras.models.load_model(SAGITTAL_MODEL_SESSION_PATH + 'best_model.h5', 
                                        custom_objects = my_custom_objects)
-    
-    model_axial = tf.keras.models.load_model(AXIAL_MODEL_SESSION_PATH + 'best_model.h5', 
-                                       custom_objects = my_custom_objects)
-    
-    model_coronal = tf.keras.models.load_model(CORONAL_MODEL_SESSION_PATH + 'best_model.h5', 
-                                       custom_objects = my_custom_objects)
+    if AXIAL_MODEL_SESSION_PATH is not None:
+        model_axial = tf.keras.models.load_model(AXIAL_MODEL_SESSION_PATH + 'best_model.h5', 
+                                           custom_objects = my_custom_objects)
+    if CORONAL_MODEL_SESSION_PATH is not None:
+        model_coronal = tf.keras.models.load_model(CORONAL_MODEL_SESSION_PATH + 'best_model.h5', 
+                                           custom_objects = my_custom_objects)
         
-    PATH = '/media/HDD/MultiAxial/Data/Processed_New_MCS/MRI/'
-    SEG_PATH = '/media/HDD/MultiAxial/Data/Processed_New_MCS/GT/'
-    anterior_commissure_df = pd.read_csv('/media/HDD/MultiAxial/Data/Processed_New_MCS_AnteriorCommissure.csv')
+    scan = '/media/HDD/MultiAxial/Data/Processed_New_MCS/MRI/PA020_1mm_ras.nii'
+    segmentation = '/media/HDD/MultiAxial/Data/Processed_New_MCS/GT/PA020_segmentation_fixed.nii'
+    anterior_commissure = [88, 135, 157]
        
+   
+    print(scan)
+    nii = nib.load(scan)
+    nii_seg = nib.load(segmentation)
+
+    print(nii.shape)
     
-    scans = [PATH + x for x in os.listdir(PATH)]
-    labels= [SEG_PATH + x for x in os.listdir(SEG_PATH)]
+    subject = scan.split('/')[-1].split('_')[0].replace('.nii','')
     
+    if subject.startswith('r'):
+        subject = subject[1:]
     
-    OUTPUT_PATH = '/home/deeperthought/Projects/Multiaxial/predictions/'
+
+    nii_out, nii_seg_out, coords, anterior_commissure = preprocess_head_MRI(nii, nii_seg, anterior_commissure=anterior_commissure)  
+
+    img = nii_out.get_fdata()
+    p95 = np.percentile(img, 95)
+    img = img/p95
+    img = np.expand_dims(img, axis=-1)
     
-    if not os.path.exists(OUTPUT_PATH):
-        os.makedirs(OUTPUT_PATH)
-       
-        
-    for scan in scans[:1]:
-        print(scan)
-        nii = nib.load(scan)
-        print(nii.shape)
-        
-        subject = scan.split('/')[-1].split('_')[0].replace('.nii','')
-        
-        if subject.startswith('r'):
-            subject = subject[1:]
-        
-        segmentation = [x for x in labels if subject in x][0]
-        nii_seg = nib.load(segmentation)
-        
-        if np.any(anterior_commissure_df['MRI'].str.contains(subject)):
-            print(f'found pre stored anterior commissure for subj: {subject}')
-            prestored_anterior_commissure = anterior_commissure_df.loc[anterior_commissure_df['MRI'].str.contains(subject)][['x','y','z']].values[0]
-        
-        nii_out, nii_seg_out, coords, anterior_commissure = preprocess_head_MRI(nii, nii_seg, anterior_commissure=prestored_anterior_commissure)  
+    coords = coords[:,:,:,:3]
+    coords = coords/256.
     
-        img = nii_out.get_fdata()
-        p95 = np.percentile(img, 95)
-        img = img/p95
-        
-        coords = coords/256.
-        
-        yhat = model_sagittal.predict([img, coords])
-        
+    img.shape
+    coords.shape    
+    yhat = model_sagittal.predict([img, coords], batch_size=1)
+    
+    yhat.shape
+    
+    model_segmentation = np.argmax(yhat, axis=-1)        
+    
+    INDEX = 100
+    plt.subplot(121)
+    plt.imshow(np.rot90(img[INDEX]))
+    plt.subplot(122)
+    plt.imshow(np.rot90(model_segmentation[INDEX]))
